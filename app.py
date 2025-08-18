59d12d93e7c9973a5aae1a9f15cefd4aa711f63a
Korvah Harper Tarnue <103341154+2010601151@users.noreply.github.com>
2025-08-18 16:46:44 +0300

    app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ animation: fadeIn 0.6s ease-in-out; }
 h1,h2,h3,h4,h5,h6 { color: #000000 !important; font-weight:bold; }
 div.stButton > button { background-color:#6a1b9a; color:black !important; font-weight:bold; border:none; border-radius:5px; padding:0.4em 1em; }
 div.stButton > button:hover { background-color:#4a0072; color:black !important; }
-input,textarea,select { border:1px solid #6a1b9a !important; color:#000000 !important; font-weight:bold; }
+input,textarea,select { border:1px solid #6a1b9a !important; color:#ffffff !important; font-weight:bold; }
 label,.stFileUploader label { color:#000000 !important; font-weight:bold; }
 table { border:2px solid #6a1b9a !important; border-collapse:collapse !important; color:#000000 !important; }
 thead tr th { background-color:#6a1b9a !important; color:black !important; font-weight:bold !important; }
@@ -334,3 +334,4 @@ if page=="📈 Analytics":
             st.markdown("<p class='notification'>No results found for analytics.</p>", unsafe_allow_html=True)
     else:
         st.markdown("<p class='notification'>No results folder found for analytics.</p>", unsafe_allow_html=True)
+
